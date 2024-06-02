e5bcfb1354fe511ba56c9b564f1e83f54a3af0c8
jeppuka <jesper.k.kahari@gmail.com>
2024-06-02 14:38:40 +0100

    hourly telegram nessage update

--- live_crypto_data.py
+++ live_crypto_data.py
@@ -1,10 +1,13 @@
 import requests
 import os
+import matplotlib.pyplot as plt
+from datetime import datetime
+import mplfinance as mpf
+import pandas as pd
+import matplotlib.dates as mdates
+
 api_key = os.getenv("CRYPTO_COMPARE_API_KEY")
-print(api_key)
-
-
-
+# print(api_key) # Uncomment this line if you want to print the API key
 
 def get_current_price(symbol): #returns the current price of a crypto
     url = f"https://min-api.cryptocompare.com/data/price?fsym={symbol}&tsyms=USD"
@@ -12,13 +15,20 @@ def get_current_price(symbol): #returns the current price of a crypto
     data = response.json()
     return data["USD"]
 
-
-
 def get_hourly_data(symbol): #returns the 60 minute datapoints for a crypto
     url = f'https://min-api.cryptocompare.com/data/v2/histominute?fsym={symbol}&tsym=USD&limit=59'
     response = requests.get(url)
     data = response.json()
-    prices = [entry['close'] for entry in data['Data']['Data']]
+
+    high = [entry['high'] for entry in data['Data']['Data']]
+    low = [entry['low'] for entry in data['Data']['Data']]
+    open = [entry['open'] for entry in data['Data']['Data']]
+    close = [entry['close'] for entry in data['Data']['Data']]
+
+    times = [entry['time'] for entry in data['Data']['Data']]
+    dates = [datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S') for time in times]
+    
+    prices = {'open': open, 'high': high, 'low': low, 'close': close, 'dates': dates}
     return prices
 
 def get_minute_price(symbol, period): #get a minute price for a specified number of minutes
@@ -29,7 +39,29 @@ def get_minute_price(symbol, period): #get a minute price for a specified number
     response = requests.get(url)
     data = response.json()
     prices = [entry['close'] for entry in data['Data']['Data']]
-    return prices
 
-print(get_minute_price("ETH", 10))
+    times = [entry['time'] for entry in data['Data']['Data']]
+    dates = [datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S') for time in times]
+    return prices, dates
 
+# Get hourly data for Ethereum
+
+
+def generate_hourly_plot(ticker):
+    hourly_data = get_hourly_data(ticker)
+    
+    # Converting the data into a DataFrame
+    df = pd.DataFrame(hourly_data)
+    df['dates'] = pd.to_datetime(df['dates'])
+    df.set_index('dates', inplace=True)
+    
+    # Set the style for the plot
+    mc = mpf.make_marketcolors(up='g', down='r', wick={'up': 'g', 'down': 'r'}, edge={'up': 'g', 'down': 'r'}, volume='in')
+    s = mpf.make_mpf_style(marketcolors=mc, gridstyle='-', y_on_right=False)
+    
+    # Plotting the candlestick chart
+    mpf.plot(df, type='candle', style=s, title=f'Hourly {ticker} Prices', ylabel='Price (USD)',
+             datetime_format='%H:%M', xrotation=45, savefig=dict(fname=f'{ticker}_hourly.jpg', dpi=300))
+
+
+generate_hourly_plot("ETH")
--- send_notifications.py
+++ send_notifications.py
@@ -1,8 +1,8 @@
 import time
 import requests
 import os
-from live_crypto_data import get_current_price
-
+from live_crypto_data import get_current_price, generate_hourly_plot
+import schedule
 
 def send_telegram_message(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
@@ -16,20 +16,38 @@ def send_telegram_message(token, chat_id, message):
     response = requests.post(url, json=payload, headers=headers)
     return response.json()
 
-
+def send_telegram_image(token, chat_id, image_path):
+    url = f'https://api.telegram.org/bot{token}/sendPhoto'
+    with open(image_path, 'rb') as image_file:
+        data = {
+            'chat_id': chat_id
+        }
+        files = {
+            'photo': image_file
+        }
+        response = requests.post(url, data=data, files=files)
+    return response.json()
 
 telegram_token = os.getenv("TELEGRAM_TOKEN")
 telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
-def run_indefinitely(interval):
-    while True:
-        price = get_current_price("ETH")
-        message = f"The current price of Ethereum is (USD): {price} "
+def send_notifications():
+    tickers = ["ETH", "BTC"]
+    for ticker in tickers:
+        price = get_current_price(ticker)
+        message = f"The current price of {ticker} is (USD): {price}$"
+        image = generate_hourly_plot(ticker)
         send_telegram_message(telegram_token, telegram_chat_id, message)
-        time.sleep(interval)
+        send_telegram_image(telegram_token, telegram_chat_id, f"{ticker}_hourly.jpg")
 
 # Run tasks every minute (60 seconds)
-run_indefinitely(60)
+schedule.every().hour.at(":00").do(send_notifications)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+#print(send_telegram_message(telegram_token, telegram_chat_id, "plot.jpg"))
+
 
 
 
